ccfaa191d71684ff7be3f953b9cbe6c748692a32
Ilia <30232564+umnovI@users.noreply.github.com>
2024-04-04 00:39:28 +0300

    Sort imports in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use anyhow::Result;
+use clap::Parser;
 use std::time::Duration;
 
 mod app;
 mod errors;
 mod tui;
 mod utils;
-use anyhow::Result;
-use clap::Parser;
 
 /// TUI for your Taskfile
 #[derive(Parser)]
